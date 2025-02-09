bbccc9383d3cfc2b2d61df0ab92b7e7de83d6eac
SahillRazaa <connectwithsahil007@gmail.com>
2025-02-09 10:20:02 +0530

    changes to resume link

--- src/Components/Aboutme.jsx
+++ src/Components/Aboutme.jsx
@@ -190,7 +190,7 @@ const Aboutme = () => {
                             <Desc1>Hey there, I'm Sahil Raza Ansari, currently a third-year student pursuing BTech in Electronics and Communication Engineering (ECE). Since my early school days, I've been captivated by the world of coding, which ignited a passion for software development alongside my academic pursuits.</Desc1>
                             <Desc2>Despite my main focus on ECE, my curiosity for software development has been unrelenting. I've dedicated considerable time to exploring various programming languages and diving into coding projects that fuel my learning and growth.</Desc2>
                             
-                            <Link to="https://www.canva.com/design/DAGU16-wyWk/vYWqWs6nihJe9Hvwph9UAQ/view?utm_content=DAGU16-wyWk&utm_campaign=designshare&utm_medium=link&utm_source=editor" target='_blank'>
+                            <Link to="https://drive.google.com/file/d/1utNt7A8e2_fHIahptEw_sNTZrsieCCGL/view?usp=sharing" target='_blank'>
                                 <ButtonCont><ResumeButton >Resume</ResumeButton></ButtonCont>
                             </Link>
                         </InfoContainer>
